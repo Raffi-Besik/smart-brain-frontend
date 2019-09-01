ac680fa950edc4cb4d9c57b37c48ff6f23ce89d4
Raffi Besiktasliyan <raffi.besik@hotmail.com>
2019-09-01 22:39:42 +0200

    Start of front-end

---
+++ src/components/rank/rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const rank = () => {
+    return (
+        <div>
+            <div className='black f3'>
+                {'Raffi, your current rank is...'}
+            </div>
+            <div className='black f1'>
+                {'#4'}
+            </div>
+        </div>
+    )
+};
+
+export default rank;
